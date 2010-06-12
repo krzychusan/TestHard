b5c4a1b739c221c196ed55edbcd17c5b3ea6ed09
ravd <r.dziadek@gmail.com>
2010-06-12 22:48:50 +0200

    Fix compilation error.

--- testhard/lib/helpers.py
+++ testhard/lib/helpers.py
@@ -8,5 +8,4 @@ available to Controllers. This module is available to templates as 'h'.
 
 from webhelpers.html import literal
 from webhelpers.html.tags import *
-from webhelpers.html.secure_form import secure_form
 
